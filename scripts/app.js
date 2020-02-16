e266c3c85d3125d97ee0b11891ca9b12a514549a
Noo8 Cod3r <brent.s.rodriguez@gmail.com>
2020-02-15 22:43:37 -0600

    JS Update

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,8 @@ const chatList = document.querySelector('.chat-list')
 
 
 // Class Instances
-const chatUI = new chatUI(chatList)
-const chatroom = new Chatroom('general', 'Greg')
+const chatUI = new ChatUI(chatList)
+const chatroom = new Chatroom('gaming', 'Greg')
 
 
 // Get and Render Chats
